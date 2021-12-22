1177e3e5cac1c08b2d23968df8eab871b65e5d78
guozifeng <1953409252@qq.com>
2021-12-22 16:28:28 +0800

    modify

--- src/main/java/com/test/alldemo/controller/StockController.java
+++ src/main/java/com/test/alldemo/controller/StockController.java
@@ -4,8 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author admin
+ */
 @RestController
 @RequestMapping("/seckill")
 @Slf4j
-public class ScoktController {
+public class StockController {
 }
