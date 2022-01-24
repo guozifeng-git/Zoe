d4bfd7e9d43bbf6b0f0dbba1729cdf75534de682
guozifeng-git <58731265+guozifeng-git@users.noreply.github.com>
2022-01-24 15:07:03 +0800

    add message modul

---
+++ message-web/src/main/java/com/gzf/messageweb/MessageWebApplication.java
@@ -0,0 +1,13 @@
+package com.gzf.messageweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageWebApplication.class, args);
+	}
+
+}
---
+++ message-web/src/test/java/com/gzf/messageweb/MessageWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gzf.messageweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
